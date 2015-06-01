05aed9dc6cf3cf7dcee14da9caa197f7ab71ee7c
mrkcsc <miguel@miguelgaeta.com>
2015-06-01 14:20:28 -0700

    Initial commit of project.

---
+++ easy-preference/app/src/main/java/com/miguelgaeta/easypreference/app/TestActivity.java
@@ -0,0 +1,16 @@
+package com.miguelgaeta.easypreference.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.e("STUFF", "MORE STUFF");
+    }
+}
---
+++ easy-preference/app/src/main/java/com/miguelgaeta/easypreference/app/TestApplication.java
@@ -0,0 +1,15 @@
+package com.miguelgaeta.easypreference.app;
+
+import android.app.Application;
+
+/**
+ * Created by mrkcsc on 3/28/15.
+ */
+public class TestApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
