1a99aaf1f197b9246c033ea0d4113f7bfabfa586
mrkcsc <miguel@miguelgaeta.com>
2015-06-01 20:11:44 -0700

    Added a basic test.

--- easy-prefs/app/src/main/java/com/miguelgaeta/easyprefs/app/TestActivity.java
+++ easy-prefs/app/src/main/java/com/miguelgaeta/easyprefs/app/TestActivity.java
@@ -4,13 +4,40 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.miguelgaeta.easyprefs.EasyPrefs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 public class TestActivity extends AppCompatActivity {
+
+    private static final EasyPrefs<List<TestClass>> pref1 = EasyPrefs.create("DA_KEY", new ArrayList<>());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.e("STUFF", "MORE STUFF");
+        Log.e("TEST: ", "Lol: " + pref1.get());
+
+        List<TestClass> testClass = new ArrayList<>();
+
+        testClass.add(TestClass.create("one"));
+        testClass.add(TestClass.create("two"));
+
+        pref1.set(testClass);
+
+
+        Log.e("TEST: ", "Lol: " + pref1.get());
+    }
+
+    @ToString @Getter @Setter @AllArgsConstructor(staticName = "create")
+    private static class TestClass {
+
+        public String test;
     }
 }
