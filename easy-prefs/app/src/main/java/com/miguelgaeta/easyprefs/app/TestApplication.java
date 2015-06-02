4adb73275c37b47d4d6734d58e1d43e3002ed3ac
mrkcsc <miguel@miguelgaeta.com>
2015-06-01 20:11:44 -0700

    Added a basic test.

--- easy-prefs/app/src/main/java/com/miguelgaeta/easyprefs/app/TestApplication.java
+++ easy-prefs/app/src/main/java/com/miguelgaeta/easyprefs/app/TestApplication.java
@@ -2,6 +2,8 @@ package com.miguelgaeta.easyprefs.app;
 
 import android.app.Application;
 
+import com.miguelgaeta.easyprefs.EasyPrefs;
+
 /**
  * Created by mrkcsc on 3/28/15.
  */
@@ -11,5 +13,6 @@ public class TestApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        EasyPrefs.getConfig().init(this);
     }
 }
